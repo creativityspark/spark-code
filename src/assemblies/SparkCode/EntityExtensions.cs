7cd1e595fc69dc076ff7213aee2acceb84085e3a
Cris Fernandez <crisfervil@gmail.com>
2025-12-02 23:12:41 +0100

    Added parsing of entity collection

--- src/assemblies/SparkCode/EntityExtensions.cs
+++ src/assemblies/SparkCode/EntityExtensions.cs
@@ -6,6 +6,25 @@ namespace SparkCode
 {
     public static class EntityExtensions
     {
+        public static string ToJson(this EntityCollection entities)
+        {
+            using (var stringWriter = new System.IO.StringWriter())
+            {
+                var jw = new JsonTextWriter(stringWriter);
+                jw.WriteStartArray();
+
+                foreach (var entity in entities.Entities)
+                {
+                    jw.WriteStartObject();
+                    WriteEntity(jw, stringWriter, entity);
+                    jw.WriteEndObject();
+                }
+
+                jw.WriteEndArray();
+                return stringWriter.ToString();
+            }
+        }
+
         public static string ToJson(this Entity entity)
         {
             using (var stringWriter = new System.IO.StringWriter())
@@ -28,6 +47,16 @@ namespace SparkCode
                     writer.WriteStartObject();
                     WriteEntity(writer, sw, (Entity)entity.Attributes[key]);
                     writer.WriteEndObject();
+                }
+                else if (entity.Attributes[key] is AliasedValue)
+                {
+                    var aliasedValue = (AliasedValue)entity.Attributes[key];
+                    writer.WriteValue(aliasedValue.Value);
+                }
+                else if (entity.Attributes[key] is OptionSetValue)
+                {
+                    var optionSetValue = (OptionSetValue)entity.Attributes[key];
+                    writer.WriteValue(optionSetValue.Value);
                 }
                 else
                 {
