a81c9be2206b214d7600f4b4788c67ca6e17679d
Cris Fernandez <crisfervil@gmail.com>
2025-12-13 11:42:57 +0100

    Added conversion to integer custom API

--- src/assemblies/SparkCode/Number/ToInteger.cs
+++ src/assemblies/SparkCode/Number/ToInteger.cs
@@ -8,11 +8,10 @@ namespace SparkCode.API.Text
     {
         public void Execute(IServiceProvider serviceProvider)
         {
-            var ctx = new SparkCode.Context(serviceProvider);
+            var ctx = new Context(serviceProvider);
 
             // API Inputs
             string text = ctx.GetInputParameter<string>("Text", true);
-
 
             // API Outputs
             ctx.SetOutputParameter("Results", int.Parse(text));
