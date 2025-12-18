8c4d782886818c1d1ad647112512f6d126359261
Cris Fernandez <crisfervil@gmail.com>
2025-12-18 17:14:55 +0100

    Added integration tests

---
+++ src/assemblies/SparkCode.API.Tests/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace SparkCode.API.Tests
+{
+    public class Context
+    {
+        private static IOrganizationService _service;
+
+        public static IOrganizationService GetService()
+        {
+            // Get connection string from environment variable
+            var connectionString = Environment.GetEnvironmentVariable("DATAVERSE_CONNECTION_STRING_TESTS");
+            if(_service == null)
+            {
+                _service = new ServiceClient(connectionString);
+            }
+            return _service;
+        }
+    }
+}
---
+++ src/assemblies/SparkCode.API.Tests/Text/ParseURLTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace SparkCode.API.Tests.Text
+{
+    public class ParseURLTests
+    {
+        [Fact]
+        public void ParseURL_ValidUrl_Returns_Parsed_Components()
+        {
+            var service = Context.GetService();
+            var url = "https://example.com:8080/path/to/resource?query=param#fragment";
+            var result = service.Execute(new OrganizationRequest("csp_Text.ParseURL")
+            {
+                Parameters = new ParameterCollection
+                {
+                    { "url", url }
+                }
+            });
+            Assert.Equal("https", result["scheme"]);
+            Assert.Equal("example.com", result["host"]);
+            Assert.Equal(8080, result["port"]);
+            Assert.Equal("/path/to/resource", result["path"]);
+            Assert.Equal("query=param", result["query"]);
+            Assert.Equal("fragment", result["fragment"]);
+
+        }
+    }
+}
