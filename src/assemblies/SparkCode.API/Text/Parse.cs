79d22c60aea17921d6a9a10b1dfe4fdddf63464f
Cris Fernandez <crisfervil@gmail.com>
2025-12-20 13:37:25 +0100

    Renamed ToInteger to Parse

--- src/assemblies/SparkCode.API/Text/Parse.cs
+++ src/assemblies/SparkCode.API/Text/Parse.cs
@@ -3,18 +3,18 @@ using System;
 
 namespace SparkCode.API.Text
 {
-    /// <displayName>To Integer</displayName>
+    /// <displayName>Parse</displayName>
     /// <summary>
-    /// Converts a text value to an integer
+    /// Converts a text value to diferent data types
     /// </summary>
-    /// <param name="Text" type="string">Text to be converted to integer</param>
-    /// <param name="Results" type="integer" direction="output">Text converted to integer</param>
+    /// <param name="Text" type="string">Text to be parsed</param>
+    /// <param name="Integer" type="integer" direction="output">Text converted to integer</param>
     /// <example>
     /// To convert a number in text format to an integer, add the Text input parameter with the text value "12345". 
     /// The Results output parameter will return the integer value 12345.
     /// ![How to use this custom API](docs/img/TextToInteger01.png "How to use this custom API")
     /// </example>
-    public class ToInteger : IPlugin
+    public class Parse : IPlugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
@@ -24,7 +24,7 @@ namespace SparkCode.API.Text
             string text = ctx.GetInputParameter<string>("Text", true);
 
             // API Outputs
-            ctx.SetOutputParameter("Results", int.Parse(text));
+            ctx.SetOutputParameter("Integer", int.Parse(text));
         }
 
     }
