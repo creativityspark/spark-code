a675ecfc9220f2b1d85f5164ca131c526aa151f6
Cris Fernandez <crisfervil@gmail.com>
2025-12-20 13:16:12 +0100

    Updated Unit Tests

--- src/assemblies/SparkCode.API.Tests/Context.cs
+++ src/assemblies/SparkCode.API.Tests/Context.cs
@@ -10,6 +10,9 @@ namespace SparkCode.API.Tests
 
         public static IOrganizationService GetService()
         {
+            // Use this powershell command to register the environment variable
+            // [System.Environment]::SetEnvironmentVariable('DATAVERSE_CONNECTION_STRING_TESTS', 'AuthType=ClientSecret;Url=https://url.crm.dynamics.com;AppId=XXX;ClientSecret=YYY', 'User')   
+
             // Get connection string from environment variable
             var connectionString = Environment.GetEnvironmentVariable("DATAVERSE_CONNECTION_STRING_TESTS");
             if(_service == null)
--- src/assemblies/SparkCode.API.Tests/Text/ParseURLTests.cs
+++ src/assemblies/SparkCode.API.Tests/Text/ParseURLTests.cs
@@ -10,20 +10,36 @@ namespace SparkCode.API.Tests.Text
         {
             var service = Context.GetService();
             var url = "https://example.com:8080/path/to/resource?query=param#fragment";
-            var result = service.Execute(new OrganizationRequest("csp_Text.ParseURL")
+            var output = service.Execute(new OrganizationRequest("csp_Text.ParseURL")
             {
                 Parameters = new ParameterCollection
                 {
-                    { "url", url }
+                    { "Url", url }
                 }
             });
-            Assert.Equal("https", result["scheme"]);
-            Assert.Equal("example.com", result["host"]);
-            Assert.Equal(8080, result["port"]);
-            Assert.Equal("/path/to/resource", result["path"]);
-            Assert.Equal("query=param", result["query"]);
-            Assert.Equal("fragment", result["fragment"]);
+            // Test only that there are values returned, not the actual values
+            var results = (Entity)output["Results"];
+            var query = (Entity)results["query"];
+            Assert.NotNull(results);
+            Assert.NotNull(query);
+        }
 
+
+        [Fact]
+        public void ParseURL_ValidUrl_Returns_Parsed_Components_Json()
+        {
+            var service = Context.GetService();
+            var url = "https://example.com:8080/path/to/resource?query=param#fragment";
+            var output = service.Execute(new OrganizationRequest("csp_Text.ParseURLJson")
+            {
+                Parameters = new ParameterCollection
+                {
+                    { "Url", url }
+                }
+            });
+            // Test only that there are values returned, not the actual values
+            var results = (string)output["ResultsJson"];
+            Assert.NotNull(results);
         }
     }
 }
