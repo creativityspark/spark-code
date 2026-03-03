23ce222c871e092003a7a632f58a694786bc89da
Cris Fernandez <crisfervil@gmail.com>
2026-03-03 17:31:11 +0100

    Added RunSQL Unit Tests

---
+++ src/assemblies/SparkCode.API.Tests/Dataverse/RunSQLTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SparkCode.API.Tests.Dataverse
+{
+    using Microsoft.Xrm.Sdk;
+    using Xunit;
+
+    public class RunSQLTests
+    {
+        [Fact]
+        public void RunSQL_ValidQuery_Returns_Results_Entity()
+        {
+            var service = Context.GetService();
+            var sql = "SELECT TOP 10 * FROM account";
+            var output = service.Execute(new OrganizationRequest("csp_Dataverse.RunSQL")
+            {
+                Parameters = new ParameterCollection
+                {
+                    { "SQLQuery", sql }
+                }
+            });
+            var results = output["Results"];
+            Assert.NotNull(results);
+        }
+
+        [Fact]
+        public void RunSQL_ValidQuery_Returns_ResultsJson_Json()
+        {
+            var service = Context.GetService();
+            var sql = "SELECT TOP 10 * FROM account";
+            var output = service.Execute(new OrganizationRequest("csp_Dataverse.RunSQLJson")
+            {
+                Parameters = new ParameterCollection
+                {
+                    { "SQLQuery", sql }
+                }
+            });
+            var resultsJson = output["ResultsJson"];
+            Assert.NotNull(resultsJson);
+        }
+
+        [Fact]
+        public void RunSQL_InvalidQuery_Throws_Exception()
+        {
+            var service = Context.GetService();
+            var sql = "SELECT * FROM non_existing_table";
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                service.Execute(new OrganizationRequest("csp_Dataverse.RunSQL")
+                {
+                    Parameters = new ParameterCollection
+                    {
+                        { "SQLQuery", sql }
+                    }
+                });
+            });
+        }
+    }
+}
