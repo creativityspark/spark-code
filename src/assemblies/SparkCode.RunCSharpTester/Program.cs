efe2270b2cf40acf3055cfa46d08ca2ec92cb5df
Cris Fernandez <crisfervil@gmail.com>
2026-04-29 19:04:46 +0200

    Added RunCSharpTester project to facilitate testing

---
+++ src/assemblies/SparkCode.RunCSharpTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Dynamic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SparkCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running tester...");
+            string input = "Cris";
+            string output = CSharpRunner.Run(input);
+            Console.WriteLine(output);
+            Console.WriteLine("Done.");
+        }
+    }
+
+    internal static class CSharpRunner
+    {
+        static public string Run(string input)
+        {
+            string output = null;
+            // add your code here
+            output = "Hello " + input;
+            return output;
+        }
+    }
+}
---
+++ src/assemblies/SparkCode.RunCSharpTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SparkCode.RunCSharpTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Creativity Spark Consulting SL")]
+[assembly: AssemblyProduct("SparkCode.RunCSharpTester")]
+[assembly: AssemblyCopyright("Copyright ©  2026 Creativity Spark")]
+[assembly: AssemblyTrademark("Creativity Spark")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14f15dea-4ee5-4607-a5a8-ee3235e162c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
